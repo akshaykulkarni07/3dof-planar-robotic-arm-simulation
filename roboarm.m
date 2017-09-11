44de04e69bd34c0e6df8a318b033ce5d0cc9fbc1
Akshay <akshayk.vnit@gmail.com>
2017-09-11 16:45:01 +0530

    Added translate function to generate 3x3 translation matrices and attempted to draw in final position. But, length changes and still not sure about angles.

--- roboarm.m
+++ roboarm.m
@@ -18,11 +18,41 @@ theta2 = pi / 4;
 theta3 = pi / 3;
 
 % transformation matrices
+
 % rotation matrices
 R1 = Rotate(theta1);
 R2 = Rotate(theta2);
 R3 = Rotate(theta3);
 
+% translation matrices
+% as length is same for all links, so only one is needed now.
+T2 = Translate(1);
+
+% finding second point
+Y = R1 * T2;
+Y1 = Y * [x(2); y(2); 1];
+x(2) = Y1(1);
+y(2) = Y1(2);
+
+% finding third point
+Y = R1 * T2 * R2 * T2;
+Y1 = Y * [x(3); y(3); 1];
+x(3) = Y1(1);
+y(3) = Y1(2);
+
+% finding fourth point
+Y = R1 * T2 * R2 * T2 * R3 * T2;
+Y1 = Y * [x(4); y(4); 1];
+x(4) = Y1(1);
+y(4) = Y1(2);
+
+% display points
+length1 = (((x(1) - x(2)) .^ 2) + ((y(1) - y(2)) .^ 2)) .^ (0.5);
+length2 = (((x(3) - x(2)) .^ 2) + ((y(3) - y(2)) .^ 2)) .^ (0.5);
+length3 = (((x(3) - x(4)) .^ 2) + ((y(3) - y(4)) .^ 2)) .^ (0.5);
+disp(length1);
+disp(length2);
+disp(length3);
 line([x(1), x(2)], [y(1), y(2)]);
 line([x(2), x(3)], [y(2), y(3)]);
 line([x(3), x(4)], [y(3), y(4)]);
